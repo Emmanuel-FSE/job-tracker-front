b4e84a4563a5bb3705273064819a919d1a3dd24b
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-02-28 12:42:11 +0300

    Initialization

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-teal-600 font-bold underline">
+      Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
